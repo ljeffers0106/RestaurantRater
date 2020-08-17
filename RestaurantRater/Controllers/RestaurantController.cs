f62121a3ba55563ee01440b2d7ab1f7b2b4fdde0
Lisa Jeffers <lisajeffers0106@gmail.com>
2020-08-17 16:16:04 -0400

    Added the Edit Function

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,7 +17,7 @@ namespace RestaurantRater.Controllers
             return View(_db.Restaurants.ToList());
         }
         // Get:  Restaurante/Create
-        [HttpGet]
+        
         public ActionResult Create()
         {
             return View();
@@ -58,5 +59,42 @@ namespace RestaurantRater.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        // GET: Restaurant/Edit/{id}
+        // Get an id from the user
+        // Handle if the id is null
+        // Find a Restaurant by that id
+        // If the restaurant doesn't exit
+        // Return the restaurant and the view
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
+
     }
 }
